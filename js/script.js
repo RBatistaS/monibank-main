521da93e6e2d6647a22a9bc9076b6efce393d5a1
RBatistaS <99196725+RBatistaS@users.noreply.github.com>
2023-06-30 13:32:16 -0300

    desenvolvendo valdações

---
+++ js/script.js
@@ -0,0 +1,18 @@
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificacampo(campo));
+})
+
+function verificacampo(campo) {
+    if (campo.name =="cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+
+    if (campo.name =="aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+}
